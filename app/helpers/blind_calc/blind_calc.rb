ef7377f8f9dd3d2519604c46be35d21a1834906d
Graham Bouvier <graham@freerunningtechnologies.com>
2016-08-22 16:29:46 -0700

    Now calculates exponential functions to determine blind

--- app/helpers/blind_calc/blind_calc.rb
+++ app/helpers/blind_calc/blind_calc.rb
@@ -2,41 +2,39 @@
 
 require 'rounding'
 
-def get_blinds(players, chips, demoninations, tournament_length, max_time_over, round_length, first_small_blind)
+def sensibly_round(n, denominations)
+  denominations.sort.each do |denomination|
+    if n < denomination*5
+      return n.round_to(denomination)
+    end
+  end
+  return n.round_to(denominations[-1])
+end
+
+def get_blinds(players, chips, denominations, tournament_length, round_length, first_small_blind)
   # tournament_length and round_length in minutes
   # at tournament_length ~5% of total chips can be small blind
   # at last_round ~25% of total chips can be small blind
+  extra_time = round_length*6
   total_chips = players * chips
-  smallest_denomination = demoninations.min
+  smallest_denomination = denominations.min
   number_of_rounds = tournament_length/round_length
-  extra_rounds = (max_time_over/round_length)+1
-  (0..tournament_length+max_time_over).step(round_length).each_with_index do |time, i|
-    # exponential function?
-    # y = (((tournament_length/60)x^3)/(total_chips*2))+first_small_blind
+  # http://www.maa.org/book/export/html/115405
+  # Calculate a function so that at the end of play time, small blind is 10% of
+  # total chips
+  k = (Math::log((total_chips*0.05).abs)-Math::log(first_small_blind.abs))/tournament_length
+  blinds = []
+  (0..tournament_length+extra_time).step(round_length).each_with_index do |time, i|
     round = i+1
-    if round == 1
-      small_blind = 1
-    elsif round <= number_of_rounds
-      small_blind = (((1/(tournament_length/60.0))*(time**3.65))/(total_chips*2))+(first_small_blind*2)
-    elsif round > number_of_rounds
-      small_blind = (((1/(tournament_length/60.0))*(time**3.65))/(total_chips*2))+(first_small_blind*2)
-    end
-    if small_blind < 10
-      small_blind= small_blind.round_to(1)
-    elsif small_blind < 100
-      small_blind = small_blind.round_to(10)
-    elsif small_blind < 500
-      small_blind = small_blind.round_to(50)
-    else
-      small_blind = small_blind.round_to(100)
-    end
-    big_blind = small_blind*2
-    puts "Round: #{round} @ #{time}"
-    puts "Small: #{small_blind}, Big: #{big_blind}"
+    small_blind = sensibly_round(Math::E**(k*time), denominations)
+    blinds << small_blind unless small_blind == blinds[-1]
   end
+  blinds
 end
 
-get_blinds(16, 20000, [1,5,10,25,50,100], 240, 60, 15, 1)
+get_blinds(7, 2000, [1,5,10,25,50,100,500,1000], 150, 15, 1).each_with_index do |blind, i|
+  puts "Time: #{((i+1)*15)/60.0} -- Small #{blind} -- Big #{blind*2}"
+end
 
 
 # Example (8, 2000, [1,5,10,25,50,100], (60*2.5).to_i, 60, 15, 1)
