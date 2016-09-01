1a56daf18fbb7a93b3a37bb8383ede218edea29c
Graham Bouvier <graham@freerunningtechnologies.com>
2016-08-31 17:08:12 -0700

    Show blind percent of total chips when going out in users history

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,8 @@ class UsersController < ApplicationController
   def history
     @user = User.find(params[:id])
     @games = @user.games.completed
+    @won = @games.select {|game| game.winner_type == "user" && game.winner == @user.id.to_s}
+    @went_out = @games - @won
   end
 
 end
