452436105c98f45704a03b86fc222d6e11415678
Graham Bouvier <graham@freerunningtechnologies.com>
2016-09-01 10:23:00 -0700

    Added extra lifetime statistics to history page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,16 @@ class UsersController < ApplicationController
     @games = @user.games.completed
     @won = @games.select {|game| game.winner_type == "user" && game.winner == @user.id.to_s}
     @went_out = @games - @won
+    chips_perc = @went_out.map do |game|
+      round_out = game.users_out[@user.id.to_s].to_i
+      total_chips = (game.guests.length+game.users.length)*game.chips.to_f
+      game = ((game.blinds[round_out]/total_chips)*100)
+    end
+    @chips_perc = chips_perc.inject(0.0) {|sum, game| sum + game } / chips_perc.length
+    round_aver = @went_out. map do |game|
+      game = game.users_out[@user.id.to_s].to_i+1
+    end
+    @round_aver = round_aver.inject(0.0) {|sum, game| sum + game } / round_aver.length
   end
 
 end
