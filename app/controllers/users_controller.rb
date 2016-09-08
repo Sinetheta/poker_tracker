5aaa4d8c9320415cca692c192ac8a699f28070da
Graham Bouvier <graham@freerunningtechnologies.com>
2016-09-08 10:40:04 -0700

    Moved user stats to a class

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,39 +2,50 @@ class UsersController < ApplicationController
 
   def history
     @user = User.find(params[:id])
-    players = @user.players
+    @players = @user.players.includes(:game => [:players])
 
+    @stats = UserStats.new(@players)
+  end
+end
 
-    @game_stats = []
-    won = []
-    went_out = []
-    players.each do |player|
-      if player.game.complete == true
-        game_stats = {game: player.game, round_out: player.round_out, winner: player.winner}
-        if game_stats[:round_out]
-          game_stats[:round_out] += 1
-          game_stats[:chips_perc] = (player.game.blinds[player.round_out+1]/player.game.total_chips.to_f)*100
-        end
-        @game_stats << game_stats
+class UserStats
+
+  attr_reader :round_out_average, :chips_out_perc, :win_perc
+
+  def initialize(players)
+    @players = players
+    @games_won = []
+    @games_lost = []
+    @round_out_average = nil
+    @chips_out_perc = nil
+    @win_perc = nil
+    lifetime_stats()
+  end
+
+  # Assign lifetime stats
+  def lifetime_stats
+    rounds_out = []
+    chips_round_out = []
+    @players.each do |player|
+      if player.winner
+        @games_won << player.game
+      elsif player.winner == false
+        @games_lost << player.game
+        rounds_out << player.round_out+1
+        chips_round_out << (player.game.blinds[player.round_out]/player.game.total_chips.to_f)*100
       end
     end
+    @round_out_average = rounds_out.sum / rounds_out.length.to_f
+    @chips_out_perc = chips_round_out.sum / chips_round_out.length
+    @win_perc = (@games_won.length/@players.length.to_f)*100
+  end
 
-    @game_stats.each do |game|
-      if game[:winner] == true
-        won << game
-      else
-        went_out << game
-      end
+  def player_stats(player)
+    if player.winner
+      {winner: true, round_out: nil, chips_perc: nil}
+    elsif player.winner == false
+      chips_perc = (player.game.blinds[player.round_out]/player.game.total_chips.to_f)*100
+      {winner: false, round_out: player.round_out, chips_perc: chips_perc}
     end
-
-    @stats = {}
-    @stats[:win_perc] = (won.length/@game_stats.length.to_f)*100
-    @stats[:round_out_aver] = went_out.map do |game|
-      game = game[:round_out]
-    end.inject(0.0) {|sum, x| sum+x } / went_out.length
-    @stats[:chips_perc] = went_out.map do |game|
-      game = (game[:game].blinds[game[:round_out]]/game[:game].total_chips.to_f)*100
-    end.inject(0.0) {|sum, game| sum + game } / went_out.length
-
   end
 end
