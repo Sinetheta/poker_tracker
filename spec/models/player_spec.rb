e6ecb9920f653fb7b1e9a9e249f2089eb2a96b35
Graham Bouvier <graham@freerunningtechnologies.com>
2016-09-09 11:26:55 -0700

    Added player specs for game_id and game reference

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -13,4 +13,12 @@ describe Player do
   it "cannot be owned by both a guest and a user" do
     expect(build(:player, :user => create(:user), :guest => create(:guest))).not_to be_valid
   end
+  it "has a game_id" do
+    expect(build(:player, :game_id => nil)).not_to be_valid
+  end
+  it "references the game with it's game_id" do
+    game = create(:game)
+    player = create(:player, :game_id => game.id)
+    expect(player.game).to eq(game)
+  end
 end
