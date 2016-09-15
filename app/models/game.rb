6d1e7a6e219066b6780f6f9b7d6d3cc5f64a79cc
Graham Bouvier <graham@freerunningtechnologies.com>
2016-09-15 10:17:20 -0700

    Factored blinds extending and shortening into GeneratedBlinds class

--- app/models/game.rb
+++ app/models/game.rb
@@ -61,26 +61,9 @@ class Game < ActiveRecord::Base
   protected
 
   def generate_blinds
-    blinds_generator = Blinds.new(self)
+    blinds_generator = BlindsGenerator.new(self)
     self.first_small_blind = blinds_generator.first_small_blind
     self.blinds = blinds_generator.blinds
-    game_end_blind = self.blinds.each_with_index.min_by {|blind, i| (self.total_chips*0.05 - blind).abs}
-    game_end = game_end_blind[1]*self.round_length
-    game_off_by = game_end-(self.game_length*60)
-    if game_off_by.abs >= self.round_length
-      if game_off_by < 0
-        game_off_by.divmod(self.round_length)[0].abs.times do
-          self.blinds << blinds_generator.blind_to_insert(self.blinds)
-          blinds.reject! {|blind| blind.nil?}
-          blinds.sort!
-          blinds.uniq!
-        end
-      else
-        game_off_by.divmod(self.round_length)[0].times do
-          self.blinds.delete_at(self.blinds.each_cons(3).each_with_index.sort_by {|b, i| b[2]/b[0].to_f}.first()[1]+1)
-        end
-      end
-    end
   end
 
   def generate_name
--- lib/blinds_generator.rb
+++ lib/blinds_generator.rb
@@ -17,26 +17,27 @@ class ExponentialSecant
 
 end
 
-class Blinds
+class BlindsGenerator
 
   attr_reader :blinds
-  attr_reader :name
   attr_reader :first_small_blind
 
   def initialize(game)
     @denominations = [1,5,10,25,50,100,250,500,1000,2000,5000]
     @denominations.select! {|denom| denom >= game.smallest_denomination}
-    @possible_blinds = [1,2,3,4,5,6,7,8,10,15,20,25,30,35,40,50,60,70,80,100,
-                        125,150,175,200,250,300,350,400,500,600,700,800,1000,
-                        1250,1500,1750,2000,2250,2500,3000,3500,4000,5000,
-                        6000,7000,8000,9000,10000,12000,14000,16000,18000,
+    @possible_blinds = [1,2,3,4,5,6,7,8,10,15,20,25,30,35,40,45,50,60,70,80,100,
+                        125,150,175,200,225,250,300,350,400,450,500,600,700,800,1000,
+                        1250,1500,1750,2000,2250,2500,2750,3000,3500,4000,5000,
+                        6000,7000,8000,9000,10000,11000,12000,14000,16000,18000,
                         20000,25000,30000,35000,40000,45000,50000]
     @possible_blinds.select! {|blind| blind >= game.smallest_denomination}
     @blind_at_game_end = poker_value((game.total_chips*0.05))
     @first_small_blind = generate_first_small_blind(game.chips)
     @blinds = [@first_small_blind]
     @total_chips = game.total_chips
-    generate_blinds(game.game_length, game.round_length)
+    @round_length = game.round_length
+    @game_length = (game.game_length*60).to_i
+    generate_blinds()
   end
 
   def poker_value(n)
@@ -47,14 +48,14 @@ class Blinds
     poker_value(chips*0.01)
   end
 
-  def generate_blinds(game_length, round_length)
+  def generate_blinds
 
-    blinds_function = ExponentialSecant.new(@first_small_blind, (game_length*60).to_i, @blind_at_game_end)
+    blinds_function = ExponentialSecant.new(@first_small_blind, @game_length, @blind_at_game_end)
     blinds = @blinds
     round = 0
     duplicates = 0
-    while blinds.empty? || blinds.last < (@blind_at_game_end*2)
-      time = round_length*round
+    while blinds.last < (@blind_at_game_end*2)
+      time = @round_length*round
       small_blind = poker_value(blinds_function.calculate_at_x(time))
       # If we get a blind less than the last one, set a max blind
       # Note: This will only happen with functions that have an asymptote.
@@ -71,19 +72,21 @@ class Blinds
     end
 
     blinds.pop
-    blinds << @blind_at_game_end*2
+    blinds << @blind_at_game_end
+    blinds << blinds.last*2
 
     duplicates.times do
-      blinds << blind_to_insert(blinds)
+      blinds << blind_to_insert()
       blinds.reject! { |blind| blind.nil? }
       blinds.sort!
     end
 
     @blinds = blinds.sort.uniq
+    insert_or_remove_blinds()
   end
 
-  def blind_to_insert(blinds)
-    spaces = blinds.select {|blind| blind <= @blind_at_game_end }.each_cons(2).each_with_index.map do |b, i|
+  def blind_to_insert
+    spaces = @blinds.select {|blind| blind <= @blind_at_game_end }.each_cons(2).each_with_index.map do |b, i|
       if b[1]-b[0] >= 2*@first_small_blind
         [b[1]/b[0].to_f, i]
       end
@@ -93,12 +96,27 @@ class Blinds
     if space.nil?
       return nil
     else
-      poker_value(((blinds[space[1]+1]-blinds[space[1]])/2.0)+blinds[space[1]])
+      poker_value(((@blinds[space[1]+1]-@blinds[space[1]])/2.0)+@blinds[space[1]])
     end
   end
 
   # Insert or remove blinds in order to achieve desired game_length
   def insert_or_remove_blinds
-   # Not yet implemented
+    measured_game_length = @blinds.index(@blind_at_game_end)*@round_length
+    time_off_by = measured_game_length-@game_length
+    if time_off_by.abs >= @round_length
+      if time_off_by < 0
+        time_off_by.abs.divmod(@round_length)[0].times do
+          @blinds << blind_to_insert()
+          @blinds.reject! { |blind| blind.nil? }
+          @blinds.sort!
+          @blinds.uniq!
+        end
+      else
+        time_off_by.divmod(@round_length)[0].times do
+          @blinds.delete_at(@blinds.each_cons(3).each_with_index.sort_by {|b, i| b[2]/b[0].to_f}.first()[1]+1)
+        end
+      end
+    end
   end
 end
