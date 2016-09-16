b4c8607f4cabdbb3f412ecd5f98d15ccc33d737b
Graham Bouvier <graham@freerunningtechnologies.com>
2016-09-16 11:49:35 -0700

    Fixed a typo that prevented seeds.rb from running

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,5 @@ include FactoryGirl::Syntax::Methods
 50.times do |count|
   puts "Creating game ##{count+1}"
   game = create(:game)
-  puts "Game created: #{debug game}"
+  puts "Game created: #{game.inspect}"
 end
