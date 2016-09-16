45d17a44aa8e76d88637555fdffaf48181e4a351
Graham Bouvier <graham@freerunningtechnologies.com>
2016-09-15 17:12:10 -0700

    Added some puts to track the running of seed and inspect games being factoried

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,8 @@ require "factory_girl"
 
 include FactoryGirl::Syntax::Methods
 
-50.times { create(:game) }
+50.times do |count|
+  puts "Creating game ##{count+1}"
+  game = create(:game)
+  puts "Game created: #{debug game}"
+end
