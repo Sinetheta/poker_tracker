382040d6c28b6f6131c29dd72eb0dd818290ff38
Graham Bouvier <graham@freerunningtechnologies.com>
2016-09-19 16:11:30 -0700

    Expanded capabilities of grabbing pizza data

--- lib/pizza_order.rb
+++ lib/pizza_order.rb
@@ -3,42 +3,73 @@
 require 'mechanize'
 require 'pry'
 
-class Pizza
-
-  attr_reader :website
-  attr_reader :classic
-  attr_reader :premium
-
-  def initialize
-    @website = ""
-    @classic = url_from_attributes(@website, :mnuid_it => 124566)
-    @premium = url_from_attributes(@website, :mnuid_it => 124586)
-    @salads = url_from_attributes(@website, :mnuid_it => 124591)
-    @mini_meals = url_from_attributes(@website, :mnuid_it => 124581)
-    @donairs = url_from_attributes(@website, :mnuid_it => 124571)
-    @sides = url_from_attributes(@website, :mnuid_it => 124596)
-    @drinks = url_from_attributes(@website, :mnuid_it => 124576)
-    @mech = Mechanize.new
+def url_from_attributes(base_url, path, **attributes)
+  url = base_url + path + "?"
+  attributes.each do |attribute, value|
+    url << "&" unless url[-1] == "?"
+    url << attribute.to_s + "=" + value.to_s
   end
+  url
+end
 
-  def url_from_attributes(base_url, **attributes)
-    url = base_url + "?"
-    attributes.each do |attribute, value|
-      url << "&" unless url[-1] == "?"
-      url << attribute.to_s + "=" + value.to_s
-    end
-    url
-  end
+class PizzaPage
 
-  def list_pizzas
-    pizzas = []
-    @mech.get(@classic) do |page|
-      pizzas += page.links_with(:class => "menudetails_item_name_link").select { |link| !link.text.empty? }
-    end
-    @mech.get(@premium) do |page|
-      pizzas += page.links_with(:class => "menudetails_item_name_link").select { |link| !link.text.empty? }
-    end
-    pizzas
+  attr_reader :webpage, :menu_path, :item_path, :categories, :mech
+
+  def initialize(webpage, menu_path, item_path, categories)
+    @mech = Mechanize.new()
+    @webpage = webpage
+    @menu_path = menu_path
+    @item_path = item_path
+    @categories = categories
+    @categories = @categories.map { |name, mnuid_it| Category.new(name, mnuid_it, self) }
   end
 
 end
+
+class Category
+
+  attr_reader :name, :mnuid_it, :page, :products, :url
+
+  def initialize(name, mnuid_it, page)
+    @name = name
+    @mnuid_it = mnuid_it
+    @page = page
+    @products = []
+    @url = url_from_attributes(page.webpage, page.menu_path, {:mnuid_it => @mnuid_it})
+    find_products
+  end
+
+  def find_products
+    products = []
+    @page.mech.get(@url) do |page|
+      page.links_with(:class => "menudetails_item_name_link").select { |link| !link.text.empty? }.each do |product|
+        @products << Product.new(product.text, product.href[/\d{7}/], self)
+      end
+    end
+  end
+
+end
+
+class Product
+
+  attr_reader :name, :category, :url
+
+  def initialize(name, iid_it, category)
+    @name = name
+    @iid_it = iid_it
+    @category = category
+    @url = url_from_attributes(category.page.webpage, category.page.item_path, {:mnuid_it => category.mnuid_it, :iid_it => @iid_it})
+  end
+
+  def find_options
+    form = nil
+    @category.page.mech.get(@url) do |page|
+      form = page.form('crtitmfrm')
+    end
+    form
+  end
+
+end
+
+binding.pry
