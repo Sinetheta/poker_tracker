3aad393dd296a3972275f5843e8a1da89820f319
Graham Bouvier <graham@freerunningtechnologies.com>
2016-09-19 13:10:40 -0700

    Now shows earnings after game based on buy-in

--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -2,6 +2,7 @@
 FactoryGirl.define do
   factory :game do |f|
     denominations = [1,5,10,25,50,100,250,500,1000]
+    f.buy_in { Random.rand(10000)+1 }
     f.smallest_denomination { denominations.sample }
     f.game_length { (Random.rand(96)+8)*0.25 }
     f.chips { Random.rand(10000)+1+(2*smallest_denomination) }
