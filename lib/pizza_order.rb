2f0e0377955dec19f8650071413cbbdaaa8317e8
Graham Bouvier <graham@freerunningtechnologies.com>
2016-09-19 17:39:10 -0700

    Added more parsing of options for pizza ordering

--- lib/pizza_order.rb
+++ lib/pizza_order.rb
@@ -14,7 +14,7 @@ end
 
 class PizzaPage
 
-  attr_reader :webpage, :menu_path, :item_path, :categories, :mech
+  attr_reader :webpage, :menu_path, :item_path, :categories, :mech, :browser
 
   def initialize(webpage, menu_path, item_path, categories)
     @mech = Mechanize.new()
@@ -53,21 +53,73 @@ end
 
 class Product
 
-  attr_reader :name, :category, :url
+  attr_reader :name, :category, :url, :form, :fields
 
   def initialize(name, iid_it, category)
     @name = name
     @iid_it = iid_it
     @category = category
     @url = url_from_attributes(category.page.webpage, category.page.item_path, {:mnuid_it => category.mnuid_it, :iid_it => @iid_it})
+    @form = find_form
+    @fields = [text_fields, size_choices, crust_choices, crust_options, sauce_options]
   end
 
-  def find_options
+  def find_form
     form = nil
     @category.page.mech.get(@url) do |page|
       form = page.form('crtitmfrm')
     end
     form
+  end
+
+  def text_fields
+    text_fields = {}
+    @form.fields.select do |field|
+      field.class != Mechanize::Form::Hidden
+    end.each do |field|
+      text_fields[field.name.to_sym] = field
+    end
+    text_fields
+  end
+
+  def size_choices
+    size_choices = {}
+    @form.radiobuttons.select do |radio|
+      radio.name == "olszid"
+    end.map do |radio|
+      size_choices[radio.text.downcase.gsub(/\$/, "").to_sym] = radio
+    end
+    size_choices
+  end
+
+  def crust_choices
+    crust_choices = {}
+    @form.radiobuttons.select do |radio|
+      radio.name == "mcctstids1"
+    end.map do |radio|
+      crust_choices[radio.text.split(" ")[0].downcase.gsub(/\'|\"/, "").to_sym] = radio
+    end
+    crust_choices
+  end
+
+  def crust_options
+    crust_options = {}
+    @form.radiobuttons.select do |radio|
+      radio.name == "mcctstids2"
+    end.map do |radio|
+      crust_options[radio.text.downcase.tr(" ", "_").gsub(/\'|\"/, "").to_sym] = radio
+    end
+    crust_options
+  end
+
+  def sauce_options
+    sauce_options = {}
+    @form.radiobuttons.select do |radio|
+      radio.name == "mcctstids3"
+    end.map do |radio|
+      sauce_options[radio.text.downcase.tr(" ", "_").gsub(/\'|\"/, "").to_sym] = radio
+    end
+    sauce_options
   end
 
 end
