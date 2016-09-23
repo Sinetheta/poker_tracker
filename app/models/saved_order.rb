075444c3fdf8a313fd963e087f787582fffaebf2
Graham Bouvier <graham@freerunningtechnologies.com>
2016-09-23 14:03:41 -0700

    Added some simple verification to Order creation

--- app/models/saved_order.rb
+++ app/models/saved_order.rb
@@ -4,4 +4,20 @@ class SavedOrder < ActiveRecord::Base
 
   serialize :order, Array
 
+  validate :products_valid
+
+  private
+
+  def products_valid
+    self.order.each do |product_selection|
+      if product_selection.class != Hash
+        errors.add(:order_misformed, "The order you submitted is not correctly formatted")
+      else
+        if Product.find_by(name: product_selection.keys[0]).nil?
+          errors.add(:invalid_product, "One of the products you entered is invalid")
+        end
+      end
+    end
+  end
+
 end
